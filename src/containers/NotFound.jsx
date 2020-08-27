9ec18cc37e853469e57f5d902fc14819e186e9f0
Jorge Leyva <jassan851031@gmail.com>
2020-08-27 16:58:42 -0400

    not found page

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../assets/styles/components/NotFound.scss'
 
 
 const NotFound = () => (
@@ -7,7 +8,7 @@ const NotFound = () => (
         <div className="err">4</div>
         <i className="far fa-question-circle fa-spin"></i>
         <div className="err2">4</div>
-        <div className="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?<p>Let's go <a href="#">home</a> and try from there.</p></div>
+        <div className="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?<p>Let's go <a href="/">home</a> and try from there.</p></div>
     </div>
 )
 
