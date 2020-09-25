d2526a5bd68ca239547ab2bcb06d588c17c70f63
Jorge Leyva <jassan851031@gmail.com>
2020-09-25 13:09:06 -0300

    infinity add bug fixed

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -15,7 +15,7 @@ const Home = ({mylist, trends, originals}) => {
         <Categories title="Mi Lista">
           <Carousel>
             {mylist.map(item =>
-              <CarouselItem key={item.id} {...item} />
+              <CarouselItem key={item.id} {...item} isList/>
             )}
           </Carousel>
         </Categories>
